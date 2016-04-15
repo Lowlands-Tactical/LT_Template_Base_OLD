cb0fae937e60e8d3ecd3039990e3101e5856f974
Sacha Ligthert <sacha@ligthert.net>
2016-04-16 01:09:52 +0200

    Refactored time-setting script from F3 to LT.

--- attribs/params.h
+++ attribs/params.h
@@ -35,9 +35,9 @@ class Params
 	class lt_param_timeOfDay
 	{
 		title = "Time of Day";
-		values[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+		values[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
 		texts[] = {"0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","Use mission settings"};
-		default = 25;
+		default = 24;
 		function = "LT_fnc_setTime";		// This function is called to apply the values
 		isGlobal = 0;						// Execute this only on the server
 		code = "lt_param_timeOfDay = %1";
