a6d1a1b2f938d35b63381487d7dc3a2792466723
Whistle <jacco.h@live.nl>
2016-04-15 11:30:04 +0200

    Added files via upload

---
+++ 3DEN/Camo/cfg3DEN.hpp
@@ -0,0 +1,28 @@
+#include "\a3\3DEN\UI\macros.inc"
+
+class Cfg3DEN
+{
+	class Attributes
+	{
+		// Base class templates
+		class Default; // Empty template with pre-defined width and single line height
+		class Title: Default
+		{
+			class Controls
+			{
+				class Title;
+			};
+		}; 
+ 
+		#include "3DEN/Camo/Camo_changer_combo.hpp";
+		
+	};
+	class LT_Section
+	{
+		displayName = "Lowlands Tactical";
+		class AttributeCategories
+		{
+				#include "3DEN/Camo/Camo_changer.hpp"			
+		};
+	};
+};
