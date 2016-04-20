fd9d612d75eca2f89943ae76d683fc559819dfab
Whistle <Jacco@lightmakers.local>
2016-04-20 10:07:20 +0200

    Combobox will now save/load values when selected and screen opened
    Thanks to R3vo

--- 3DEN/Camo/Camo_combo.hpp
+++ 3DEN/Camo/Camo_combo.hpp
@@ -1,8 +1,45 @@
 // Your attribute class
 class LT_Camo_combo: Title
 {
-	//attributeLoad = "(_this controlsGroupCtrl 100) ctrlSetText _value";
-	//attributeSave = "ctrlText (_this controlsGroupCtrl 100)";
+	//Thanks to R3vo (https://github.com/RevoArma3/3denEnhanced/blob/master/3denEnhanced/controls/garrisonCoverageCombo.hpp)
+	attributeLoad = "\
+		_ctrl = _this controlsGroupCtrl 100;\
+		_attCtrl = getText( _config >> 'control' );\
+		_staticItemsCfg = configFile >> 'Cfg3DEN' >> 'Attributes' >> _attCtrl >> 'Controls' >> 'Value' >> 'items';\
+		\
+		_fnc_setValues = {\
+			private [ '_index' ];\
+			params[ '_path', [ '_apply', true ] ];\
+			{\
+				_cfg = _x;\
+				if ( _apply ) then {\
+					_index = _ctrl lbAdd getText( _cfg >> 'text' );\
+					_ctrl lbSetData [ _index, getText( _cfg >> 'data' ) ];\
+				}else{\
+					_index = _foreachindex;\
+				};\
+				if ( !( _value isEqualType '' ) ) then {\
+					if ( _index isEqualTo _value ) then {\
+						_ctrl lbSetCurSel _index;\
+					};\
+				}else{\
+					if ( _value == getText( _cfg >> 'data' ) ) then {\
+						_ctrl lbSetCurSel _index;\
+					};\
+				};\
+			}forEach configProperties [_path,'isclass _x'];\
+		};\
+		if ( isClass _staticItemsCfg ) then {\
+			[ _staticItemsCfg, false ] call _fnc_setValues;\
+		};\
+	";
+
+	//Thanks to R3vo (https://github.com/RevoArma3/3denEnhanced/blob/master/3denEnhanced/controls/garrisonCoverageCombo.hpp)
+	attributeSave =
+	"\
+		_ctrl = _this controlsGroupCtrl 100;\
+		_ctrl lbData lbCurSel _ctrl;\
+	";
 	class Controls: Controls
 	{
 		class Title: Title{}; // Inherit existing title control. Text of any control with class Title will be changed to attribute displayName
