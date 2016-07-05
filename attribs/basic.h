12e2d799b0fa46f9590c6ecb75ae30a610c01711
Sacha Ligthert <sacha@ligthert.net>
2016-07-05 23:53:48 +0200

    Removal of stuff done from 3DEN.

--- attribs/basic.h
+++ attribs/basic.h
@@ -1,10 +1,3 @@
-disabledAI = 1;
-ShowGPS = 1;
-showCompass = 1;
-showWatch = 1;
-aiKills = 0;
-debriefing = 1;
-showMap = 1;
 disableChannels[] = {0,2,4,5,6};
 enableItemsDropping = 0;
 allowFunctionsRecompile = 1;
