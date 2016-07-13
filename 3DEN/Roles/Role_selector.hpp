d3172e524981895581796603ac4c8bf77b8e730e
Sacha Ligthert <sacha@ligthert.net>
2016-07-13 21:51:04 +0200

    Potential fix for the Role selector issue.
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck
    Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck Fuck

--- 3DEN/Roles/Role_selector.hpp
+++ 3DEN/Roles/Role_selector.hpp
@@ -4,7 +4,7 @@ class LT_role_selector
 	tooltip = "Configure the role of this unit";
 	property = "LT_role_combo_ID";
 	control = "LT_role_combo";
-	expression = "_this setVariable ['LT_unit_role',_value];";
+	expression = "_this setVariable ['LT_unit_role',_value,true];";
 	defaultValue = "custom";
 	condition = "objectBrain + objectControllable"; // Condition for attribute to appear (see the table below)
 	typeName = "STRING"; // Defines data type of saved value, can be STRING, NUMBER or BOOL. Used only when control is "Combo", "Edit" or their variants
