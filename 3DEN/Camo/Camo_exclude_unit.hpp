74fa6b409b0875a5b16ebce33ad3d13ca1402247
Whistle <jacco.h@live.nl>
2016-07-14 08:28:23 +0200

    Deze moet ook public zijn

--- 3DEN/Camo/Camo_exclude_unit.hpp
+++ 3DEN/Camo/Camo_exclude_unit.hpp
@@ -4,7 +4,7 @@ class LT_camo_exclude_unit
 	tooltip = "Exclude unit from camouflage-setter";
 	property = "LT_camo_exclude_unit_ID";
 	control = "CheckboxNumber";
-	expression = "_this setVariable ['LT_camo_exclude', _value];";
+	expression = "_this setVariable ['LT_camo_exclude', _value, true];";
 	validate = "number";
 	defaultValue = "0";
 	condition = "objectBrain + objectControllable"; // Condition for attribute to appear (see the table below)
