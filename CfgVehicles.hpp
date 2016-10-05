72baa0f26020f3fad8f11056e6fb712bdba734d4
WhistleTDNL <jacco.h@live.nl>
2016-10-05 19:13:00 +0200

    Changed include order of modules to priority

--- CfgVehicles.hpp
+++ CfgVehicles.hpp
@@ -4,8 +4,8 @@ class CfgVehicles {
 		class ArgumentsBaseUnits {
 		};
 	};
-	#include "AI\UPSMON\MissionModules\UPSMON_MainModule.hpp" // periority 2
+	#include "gear\MissionModules\LT_setItemsModule.hpp" 				// periority 1
+	#include "AI\UPSMON\MissionModules\UPSMON_MainModule.hpp" 	// periority 2
 	#include "AI\UPSMON\MissionModules\UPSMON_PatrolModule.hpp" // periority 3
-	#include "gear\MissionModules\LT_setItemsModule.hpp" // periority 1
-	#include "AI\GARRISON\MissionModules\Garrison_Module.hpp" // periority 4
+	#include "AI\GARRISON\MissionModules\Garrison_Module.hpp" 	// periority 4
 };
