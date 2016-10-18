37d0078028dfb7b8f8c168dc35e7a65eb7369fc2
Sacha Ligthert <sacha@ligthert.net>
2016-10-18 02:12:27 +0200

    Make a units static, prevent him/her/it/xer from going prone.

--- 3DEN/StaticUnit/setUnitStatic.hpp
+++ 3DEN/StaticUnit/setUnitStatic.hpp
@@ -4,8 +4,8 @@ class LT_StaticUnit
 	tooltip = "Fix the unit to this position. The unit can still fire";
 	property = "LT_StaticUnit_ID";
 	control = "Checkbox";
-	expression = "_this allowFleeing 0; _this forceSpeed 0;";
+	expression = "_this allowFleeing 0; _this forceSpeed 0; _this addEventHandler ['Animchanged',{(_this select 0) setunitpos 'up'}];";
 	defaultValue = "false";
-	condition = "objectBrain + objectControllable"; // Condition for attribute to appear (see the table below)
-	typeName = "bool"; // Defines data type of saved value, can be STRING, NUMBER or BOOL. Used only when control is "Combo", "Edit" or their variants
+	condition = "objectBrain + objectControllable";
+	typeName = "bool";
 };
